55977f26fe46ee0e464f30dec90605f69a20b8d9
Scriptopathe <josue.alvarez@hotmail.fr>
2015-03-26 20:38:52 +0100

    Added test java IA.

---
+++ Codinsa2015.Client/Codinsa2015.Client/Views/Clank/ClientJava/src/IATest.java
@@ -0,0 +1,32 @@
+import java.io.IOException;
+import java.net.UnknownHostException;
+import java.util.ArrayList;
+
+import net.codinsa2015.*;
+
+public class IATest {
+	public static void main(String[] args) throws UnknownHostException, IOException {
+		TCPHelper.Initialize("127.0.0.1", 5000, "IA Java mdr");
+		
+		State state = new State();
+
+		SceneMode mode = state.GetMode();
+		System.out.println(mode);
+		ArrayList<SpellView> spells = state.GetSpells();
+		int spellId = 0;
+		while (true)
+		{
+			ArrayList<EntityBaseView> entities = state.GetEntitiesInSight();
+			if (entities.size() != 0)
+			{
+				EntityBaseView entity = entities.get(0);
+				if (!state.IsAutoMoving())
+				{
+					System.out.println("Moving to entity " + entity.ID + ", Position = " + entity.Position.X + ", " + entity.Position.Y);
+					state.StartMoveTo(entity.Position);
+				}
+			}
+		}	
+	}
+
+}
